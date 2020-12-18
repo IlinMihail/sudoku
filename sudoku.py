36e4cdf834b7641e479da82fc5bba82b4da616e9
IlinMihail <Ihaveagreatfantasy@gmail.com>
2020-12-18 13:00:26 +0300

    Logic fixed

--- sudoku.py
+++ sudoku.py
@@ -4,39 +4,39 @@ c = True
 num = -1
 
 grid = [
-    [9, 0, 2, 6, 1, 7, 0, 0, 4],
-    [0, 1, 0, 0, 0, 0, 0, 6, 0],  # 2,3,4,5,9
-    [0, 5, 0, 0, 0, 8, 9, 0, 0],
-    [0, 0, 0, 1, 0, 0, 2, 0, 0],
-    [5, 0, 0, 0, 9, 0, 1, 4, 0],
-    [0, 9, 8, 0, 0, 0, 0, 0, 3],
-    [0, 0, 0, 0, 4, 0, 0, 0, 0],
-    [2, 0, 0, 0, 5, 0, 0, 0, 0],
-    [0, 8, 0, 0, 0, 3, 0, 0, 5]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
 
-# Ввод: матрица для вывода, вывод: вывод матрицы
+# Ввод: матрица для вывода, вывод: вывод матрицы на экран
 def show_grid(g):
     for r in range(9):
         row = g[r]
         print(row)
-
 
 # Ввод: матрица с заданием, вывод: решённая матрица
 def autosolve(g):
     solved_grid = g.copy()
     p_nums = []
     while not is_solved(solved_grid):
-        for y in range(9):
-            for x in range(9):
+        for x in range(9):
+            for y in range(9):
                 p_nums = get_nums(solved_grid, x, y)
+                print(p_nums)
                 if len(p_nums) == 1:
-                    print(p_nums)
+                    print('Уникальное число:{0} в {1},{2}'.format(p_nums[0], x, y))
                     solved_grid[y][x] = p_nums[0]
 
-        show_grid(solved_grid)
-        return solved_grid
+    show_grid(solved_grid)
+    return solved_grid
 
 
 # Ввод: -, вывод: матрица с заданием
@@ -47,16 +47,12 @@ def autogenerate():
 
 # Ввод: матрица, число и координаты ячейки, вывод: Ложь или Истина взависимости от того можно ли поставить это число на выбранных координатах
 def check_cell(g, n, x, y):
-    x -= 1
-    y -= 1
     if n > 9 or n < 1:
         return False
 
     if g[y][x] != 0:  # Проверка наличия числа на выбранной клетке
         return False
 
-    indx = -1
-    indy = -1
     for indx in range(9):  # Проверка наличия числа в строке
         if indx != x and g[y][indx] == n:
             return False
@@ -74,7 +70,7 @@ def check_cell(g, n, x, y):
 
     return True  # Возврат истины в случае если все условия соблюдены
 
-
+# Ввод: матрица и координаты ячейки, вывод: возможные числа для этой ячейки
 def get_nums(g, x, y):
     p = []
     for n in range(10):
@@ -82,17 +78,16 @@ def get_nums(g, x, y):
             p.insert(0, n)
     return p
 
-
+# Ввод: матрица, вывод: Истина или Ложь взависимости от того решена ли матрица
 def is_solved(g):
     for y in range(9):
         for x in range(9):
             if g[y][x] == 0:
                 return False
-
     return True
 
 
-print('Судоку v.1.4')
+print('Судоку v.1.5')
 show_grid(grid)
 while True:
     f = False
@@ -105,21 +100,44 @@ while True:
             f = True
         elif val == 'generate':
             autogenerate()
+        elif val == 'import':
+            c = input('Введите строку: ')
+            n = -1
+            for y in range(9):
+                for x in range(9):
+                    n += 1
+                    grid[y][x] = int(c[n])
+
         elif val == 'debug':
             # Тесты
-            print('Тесты. Должна быть только Правда')
-            show_grid(grid)
-            print(is_solved(grid) == False)
-            print(get_nums(grid, 4, 2) == [9, 5, 4, 3, 2])
-            print(check_cell(grid, 1, 8, 8) == True)
-            print(check_cell(grid, 1, 2, 2) == False)
-
+            grid_2 = [
+                [9, 0, 2, 6, 1, 7, 0, 0, 4],
+                [0, 1, 0, 0, 0, 0, 0, 6, 0],
+                [0, 5, 0, 0, 0, 8, 9, 0, 0],
+                [0, 0, 0, 1, 0, 0, 2, 0, 0],
+                [5, 0, 0, 0, 9, 0, 1, 4, 0],
+                [0, 9, 8, 0, 0, 0, 0, 0, 3],
+                [0, 0, 0, 0, 4, 0, 0, 0, 0],
+                [2, 0, 0, 0, 5, 0, 0, 0, 0],
+                [0, 8, 0, 0, 0, 3, 0, 0, 5]
+            ]
+            print('Тесты. Должна быть только Истина')
+            print('Тесты проводятся на сетке:')
+            show_grid(grid_2)
+            print(is_solved(grid_2) == False)
+            print(get_nums(grid_2, 3, 1) == [9,5,4,3,2])
+            print(check_cell(grid_2, 1, 7, 7) == True)
+            print(check_cell(grid_2, 1, 1, 1) == False)
         else:
             vals = val.split(',')
             num = int(vals[0])
             x = int(vals[1])
             y = int(vals[2])
-            if x < 10 or y < 10:
+            x -= 1
+            y -= 1
+            if x < 0 or y < 0 or x > 9 or y > 9:
+                print('Координаты должны быть не меньше 1 и не больше 9')
+            else:
                 if f == True:
                     grid[y][x] = num
                     c = False
@@ -129,8 +147,7 @@ while True:
                         grid[y][x] = num
                         c = False
                     else:
-                        print('Вы не можете поставить', num, 'здесь')
+                        print('Вы не можете поставить {0} здесь'.format(num))
 
     show_grid(grid)
     c = True
-
